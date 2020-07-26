2448457109a9da2684e110e8fe22f7894fd810ba
Kacper Turkowski <kacperturkowski27@gmail.com>
2020-07-26 16:13:13 +0200

    add a program that turns numbers into word

---
+++ SłownieOkienko/SłownieOkienko/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SłownieOkienko
+{
+    partial class Słownie
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(554, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.RESET_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 63);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(554, 80);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Słownie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 207);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Słownie";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ SłownieOkienko/SłownieOkienko/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace SłownieOkienko
+{
+    public partial class Słownie : Form
+    {
+        private static string[] unity = { "", "jeden ", "dwa ", "trzy ", "cztery ", "pięć ", "sześć ", "siedem ", "osiem ", "dziewięć " };
+        private static string[] teen = { "", "jedenaście ", "dwanaście ", "trzynaście ", "czternaście ", "piętnaście ", "szesnaście ", "siedemnaście ", "osiemnaście ", "dziewiętnaście " };
+        private static string[] dozens = { "", "dziesięć ", "dwadzieścia ", "trzydzieści ", "czterdzieści ", "pięćdziesiąt ", "sześćdziesiąt ", "siedemdziesiąt ", "osiemdziesiąt ", "dziewięćdziesiąt " };
+        private static string[] hundreds = { "", "sto ", "dwieście ", "trzysta ", "czterysta ", "pięćset ", "sześćset ", "siedemset ", "osiemset ", "dziewięćset " };
+        protected static string[,] variety = new string[,]
+                {
+                { "", "", ""                           },
+                { "tysiąc ", "tysiące ", "tysięcy "       },
+                { "milion " , "miliony " , "milionów "    },
+                { "miliard ", "miliardy ", "miliardów "   },
+                { "bilion " , "biliony " , "bilionów "    },
+                { "biliard ", "biliardy ", "biliardów "   },
+                { "trylion " , "tryliony ", "trylionów " },
+                { "tryliard ", "tryliardy ", "tryliardów "},
+                { "kwadrylion ", "kwadryliony ", "kwadrylionów " },
+                { "kwadryliard ", "kwadryliardy ", "kwadryliardów " },
+                { "kwintylion ", "kwintyliony ", "kwintylionów " },
+                { "kwintyliard ", "kwintyliardy ", "kwintyliardów"},
+                { "sekstylion ","sekstyliony ", "sekstylionów "},
+                { "sekstyliard ","sekstyliardy ", "sekstyliardów" },
+                { "septylion ","septyliony ","septylionów "},
+                { "septyliard ","septyliardów ","septyliardów " },
+                { "oktylion ","oktyliony ","oktylionów "},
+                { "oktyliard ","oktyliardy ","oktyliardów " },
+                { "nonylion ","nonyliony ","nonylionów "},
+                { "nonyliard ","nonyliardy ","nonyliardów " },
+                { "decylion ","decyliony ","decylionów "},
+                { "decyliard ","decyliardy ","decyliardów " },
+                {"wicylion ","wicyliony ","wicylionów " },
+                {"wicyliard ","wicyliardy ","wicyliardów " },
+                {"trycylion ","trycyliony ","trycylionów " },
+                {"trycliard ","trycyliardy ","trycyliardów " },
+                {"kwadragilion ","kwadragiliony ","kwadragilionów " },
+                {"kwadragiliard ","kwadragiliardy ","kwadragiliardów " },
+                {"kwinkwagilion ","kwinkwagiliony ","kwinkwagilionów " },
+                {"kwinkwagiliard ","kwinkwagiliardy ","kwinkwagiliardów " },
+                {"seskwilion ","seskwiliony ","seskwilionów " },
+                {"seskwiliard ","seskwiliardy ","seskwiliardów " },
+                {"septagilion ","septagiliony ","septagilionów " },
+                {"septagiliard ","septagiliardy ","septagiliardów " },
+                {"oktogilion ","oktogiliony ","oktogilionów " },
+                {"oktogiliard ","oktogiliardy ","oktogiliardów " },
+                {"nonagilion ","nonagiliony ","nonagilionów " },
+                {"nonagiliard ","nonagiliardy ","nonagiliardów " },
+                };
+        string caption = "Error";
+        MessageBoxButton button = MessageBoxButton.OK;
+        MessageBoxImage icon = MessageBoxImage.Warning;
+        private string słownie(string number)
+        {
+            StringBuilder result = new StringBuilder();
+            try
+            {
+                if(number[0]=='-')
+                {
+                    result.Append("minus ");
+                    number = number.Substring(1, number.Length - 1);
+                }
+            }catch(IndexOutOfRangeException) { /*Empty Text Box*/ return ""; }
+            bool check = true;
+            for (int i = 0; i < number.Length; i++) if (number[i] != '0') check = false;
+            if (check) return "zero";
+            string a, b, c;
+            string[] three;
+            if (number.Length % 3 == 0) three = new string[number.Length / 3];
+            else three = new string[(number.Length / 3) + 1];
+            int index = 0;
+            while (number.Length-3 > 0)
+            {
+                a = number.Substring(number.Length - 1);
+                number = number.Substring(0, number.Length - 1);
+
+                b = number.Substring(number.Length - 1);
+                number = number.Substring(0, number.Length - 1);
+
+                c = number.Substring(number.Length - 1);
+                number = number.Substring(0, number.Length - 1);
+
+                three[index] = c + "" + b + "" + a;
+                index++;
+            }
+            while (number.Length > 0)
+            {
+                a = number.Substring(number.Length - 1);
+                number = number.Substring(0, number.Length - 1);
+                three[index] = a + "" + three[index];
+            }
+            int[] array = new int[three.Length];
+            for (int i = 0; i < three.Length; i++)
+            {
+                try
+                {
+                    array[i] = int.Parse(three[i]);
+                }
+                catch (FormatException e)
+                {
+                    System.Windows.MessageBox.Show(e.Message, caption, button, icon);
+                    return ""; 
+                }
+            }
+            for (int i = three.Length - 1; i >= 0; i--)
+            {
+                result.Append(GetNumber(array, i));
+            }
+            return result.ToString();
+        }
+        private string GetNumber(int[] k, int i)
+        {
+            StringBuilder str = new StringBuilder();
+            int three, second, one;
+            three = k[i] / 100;
+            second = (k[i] / 10) - three * 10;
+            one = k[i] - second * 10 - three * 100;
+            if (k[i]-three*100>=11 && k[i]-three*100<=19) str.Append(hundreds[three] + teen[one]);
+            else str.Append(hundreds[three] + dozens[second] + unity[one]);
+            try
+            {
+                if (k[i] == 0) return "";
+                else if (k[i] == 1) str.Append(variety[i, 0]);
+                else if (k[i] >= 5 && k[i] <= 21) str.Append(variety[i, 2]);
+                else if (one >= 2 && one <= 4) str.Append(variety[i, 1]);
+                else str.Append(variety[i, 2]);
+            }catch(IndexOutOfRangeException e) { System.Windows.MessageBox.Show(e.Message, caption, button, icon); }
+            return str.ToString();
+        }
+        public Słownie()
+        {
+            InitializeComponent();
+            this.button1.Text = "RESET";
+            this.Text = "Słownie";
+            textBox1.Font = new Font(textBox1.Font.FontFamily, 16);
+            textBox1.Height = 16;
+            richTextBox1.Font = new Font(richTextBox1.Font.FontFamily, 16);
+        }
+
+        private void Form1_Load(object sender, EventArgs e){}
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            string Text = słownie(this.textBox1.Text);
+            this.CreateMyMultilineTextBox(Text);
+        }
+
+        public void CreateMyMultilineTextBox(String Text)
+        {
+            richTextBox1.Multiline = true;
+            richTextBox1.ScrollBars = RichTextBoxScrollBars.Vertical;
+            richTextBox1.AcceptsTab = true;
+            richTextBox1.WordWrap = true;
+            richTextBox1.Text = Text;
+        }
+
+        private void RESET_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Text = "";
+            this.textBox1.Text = "";
+        }
+    }
+
+}
