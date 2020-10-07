d56112dd82bbf841ab6ffeda636234da4a2b70c3
Kacper Turkowski <kacperturkowski27@gmail.com>
2020-10-07 13:27:53 +0200

    add the_game_of_life

---
+++ the_game_of_life/the_game_of_life/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace the_game_of_life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1159, 830);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1177, 43);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(1177, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Amount of mesh";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1177, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 58);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1177, 107);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(145, 22);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(1177, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Amount of round";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1345, 845);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Game of life";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ the_game_of_life/the_game_of_life/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace the_game_of_life
+{
+    public partial class Form1 : Form
+    {
+        int amountOfMesh = 0;
+        int widthOfMesh = 0;
+        int amountOfRound = 0;
+        private int heightOfMesh=0;
+
+        Graphics gr = null;
+        private Bitmap canvas;
+
+        Pixel[,] mesh;
+        
+        string caption = "Error";
+        MessageBoxButton button = MessageBoxButton.OK;
+        MessageBoxImage icon = MessageBoxImage.Warning;
+
+        public Form1()
+        {
+            InitializeComponent();
+            canvas = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            gr = Graphics.FromImage(canvas);
+        }
+        private void drawMesh()
+        {
+            Pen pen = new Pen(Color.FromArgb(192,192,192), 1);
+            for (int i = 0; i < amountOfMesh; i++)
+            {
+                gr.DrawLine(pen, new Point((int)(i * ((double)this.canvas.Width / amountOfMesh)), 0), new Point((int)(i * ((double)this.canvas.Width / amountOfMesh)), canvas.Height));//pion
+                gr.DrawLine(pen, new Point(0, (int)(i * ((double)this.canvas.Height / amountOfMesh))), new Point(canvas.Width, (int)(i * ((double)this.canvas.Height / amountOfMesh))));//poziom
+            }
+            gr.DrawLine(pen, new Point(0, canvas.Height - 1), new Point(canvas.Width, canvas.Height - 1));//last horizontal
+            gr.DrawLine(pen, new Point(canvas.Width - 1, 0), new Point(canvas.Width - 1, canvas.Height));//last vertical
+
+            this.widthOfMesh = (int)(2 * ((double)this.canvas.Width / amountOfMesh)) - (int)(1 * ((double)this.canvas.Width / amountOfMesh));//window width
+            this.heightOfMesh = (int)(2 * ((double)this.canvas.Height / amountOfMesh)) - (int)(1 * ((double)this.canvas.Height / amountOfMesh));//window height
+
+            this.pictureBox1.Image = (Image)canvas;
+
+        }
+        private void drawRect()
+        {
+            for(int i = 0; i < amountOfMesh; i++)
+            {
+                for(int j = 0; j < amountOfMesh; j++)
+                {
+                    if (mesh[i, j].isAlive) {
+                        this.gr.FillRectangle(new SolidBrush(Color.Blue), mesh[i, j].a, mesh[i, j].b, this.widthOfMesh,this.heightOfMesh);
+                    }
+                }
+            }
+            this.pictureBox1.Image = (Image)canvas;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            gr.Clear(Color.White);
+            if (this.textBox1.Text != "")//window is empty
+            {
+                try
+                {
+                    this.amountOfMesh = int.Parse(this.textBox1.Text);
+                        if (this.amountOfMesh < 0)
+                        {
+                            System.Windows.MessageBox.Show("Amount of mesh is too small", caption, button, icon);
+                            this.amountOfMesh = 0;
+                        }
+                    }
+                catch (FormatException exception)
+                {
+                    System.Windows.MessageBox.Show(exception.Message, caption, button, icon);
+                }
+            }
+            drawMesh();
+            mesh = new Pixel[amountOfMesh, amountOfMesh];
+            for (int i = 0; i < amountOfMesh; i++)
+            {
+                for (int j = 0; j < amountOfMesh; j++)
+                {
+                    mesh[i, j] = new Pixel((int)(i * ((double)canvas.Width / amountOfMesh)), (int)(j * ((double)canvas.Height / amountOfMesh)));
+                }
+            }
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBox4.Text != "")
+            {
+                try
+                {
+                    this.amountOfRound = int.Parse(this.textBox4.Text);
+                    if (this.amountOfRound < 0)
+                    {
+                        System.Windows.MessageBox.Show("Amount of round is too small", caption, button, icon);
+                        this.amountOfRound = 0;
+                    }
+                }
+                catch (FormatException exception)
+                {
+                    System.Windows.MessageBox.Show(exception.Message, caption, button, icon);
+                }
+            }
+        }
+        //start gry
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int neighbour = 0;
+            Pixel[,] temp= new Pixel[amountOfMesh, amountOfMesh];
+            for(int i = 0; i < amountOfMesh; i++)
+                for (int j = 0; j < amountOfMesh; j++)
+                    temp[i, j] = new Pixel(mesh[i, j].a, mesh[i, j].b, mesh[i, j].isAlive);
+          
+
+            for (int i = 0; i < this.amountOfRound; i++)
+            {
+                for (int x = 0; x < amountOfMesh; x++)
+                {
+                    for(int y = 0; y < amountOfMesh; y++)
+                    {
+                        neighbour = 0;
+                        try{ if (mesh[x - 1, y - 1].isAlive) neighbour++;}catch (IndexOutOfRangeException){ }
+                        try{ if (mesh[x - 1, y].isAlive) neighbour++;}catch (IndexOutOfRangeException) { }
+                        try{ if (mesh[x - 1, y + 1].isAlive) neighbour++;}catch (IndexOutOfRangeException) { }
+
+                        try{ if (mesh[x, y - 1].isAlive) neighbour++;} catch (IndexOutOfRangeException) { }
+                        try{ if (mesh[x, y + 1].isAlive) neighbour++;}catch (IndexOutOfRangeException) { }
+
+                        try{ if (mesh[x + 1, y - 1].isAlive) neighbour++;}catch (IndexOutOfRangeException) { }
+                        try{ if (mesh[x + 1, y ].isAlive) neighbour++;} catch (IndexOutOfRangeException) { }
+                        try{ if (mesh[x + 1, y + 1].isAlive) neighbour++;} catch (IndexOutOfRangeException) { }
+
+
+                        if (neighbour == 3) temp[x, y].born();
+                        else if (neighbour == 2 && mesh[x,y].isAlive) temp[x, y].born();
+                        else temp[x, y].kill();
+                    }
+                }
+                
+                for (int x = 0; x < amountOfMesh; x++)
+                    for (int j = 0; j < amountOfMesh; j++)
+                        mesh[x, j] = new Pixel(temp[x, j].a, temp[x, j].b, temp[x, j].isAlive);
+
+                gr.Clear(Color.White);
+                drawMesh();
+                drawRect();
+                this.pictureBox1.Image = (Image)canvas;
+                pictureBox1.Refresh();
+                Thread.Sleep(250);
+            }
+
+            System.Windows.MessageBox.Show("End", "success", button, icon);
+        }
+             
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            int xPicture = this.Left +2*this.pictureBox1.Location.X;
+            int yPicture = this.Top + 4* this.pictureBox1.Location.Y;
+            int xMouse = MousePosition.X;
+            int yMouse = MousePosition.Y;
+            int x, y;
+            x = xMouse - xPicture;
+            y = yMouse - yPicture;
+
+            x = (int)(x /(double) this.widthOfMesh);
+            y = (int)(y /(double) this.heightOfMesh);
+            try
+            {
+                if (mesh[x, y].isAlive)
+                {
+                    this.mesh[x, y].kill();
+                    gr.Clear(Color.White);
+                    drawRect();
+                }
+                else
+                {
+                        this.gr.FillRectangle(new SolidBrush(Color.Blue), mesh[x, y].a, mesh[x, y].b, this.widthOfMesh, this.heightOfMesh);
+                        this.mesh[x, y].born();
+                }
+                drawMesh();
+            }
+            catch (IndexOutOfRangeException) { }
+        }
+
+    }
+    class Pixel
+    {
+        public int a, b;
+        public bool isAlive=false;
+        public Pixel(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public Pixel(int a, int b,bool isAlive)
+        {
+            this.a = a;
+            this.b = b;
+            this.isAlive = isAlive;
+        }
+        public void born() { this.isAlive = true; }
+        public void kill() { this.isAlive = false;}
+    }
+}
